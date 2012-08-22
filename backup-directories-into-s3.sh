11685fc9a63ad8ca2a6341410904df5e96a37955
deigote <diego@toharia.com>
2012-08-22 13:16:37 +0200

    Compress a directory and upload it to s3, split needs to be added in order to support large files

---
+++ backup-directories-into-s3.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# Backup a given list of directories into S3 by compressing and splitting them (in case 
+# is necessary).
+# Depends on:
+# - aws-common.sh
+# - s3cmd
+# - tr
+# Author: diego.toharia@osoco.es - OSOCO
+
+COMMON_SCRIPT_PATH="`dirname $0`/aws-common.sh"
+if [ -f "$COMMON_SCRIPT_PATH" ] ; then
+    source $COMMON_SCRIPT_PATH
+else
+    echo "aws-common.sh not found in $COMMON_SCRIPT_PATH... Exiting now" >&2
+    exit 1
+fi
+
+USAGE_DESCRIPTION="Usage: `basename $0` -b <bucket_path>] [ -p <backup_path> ] \
+[-t <tmp_directory>] [ -d ] <directory_to_backup> [<another_directory_to_backup>]\n\
+Options description:\n\
+-b <bucket_path> The bucket where the backups will be uploaded\n\
+-p <backup_path> The path that will be prepended to each backup filename (by default, /)\n\
+-t <tmp_directory> The temporary directory to use (by default, /tmp)\n\
+-d When passed, each directory will be backuped in its own S3 directory\n\n\
+The script needs s3cmd to be installed and configured"
+
+BACKUP_PATH="/"
+TMP_DIR="/tmp/"
+function parse_params
+{
+    while getopts ":b:p:d" opt; do
+        case $opt in
+        b)
+            BUCKET_PATH="$OPTARG"
+            ;;
+        p)
+            BACKUP_PATH="$OPTARG"
+            ;;
+        t)
+            TMP_DIR="$OPTARG"
+            ;;
+        d)
+            DIRECTORY_BY_BACKUP="YES"
+            ;;
+        \?)
+            print_error "Unknown option -$OPTARG"
+            usage "$USAGE_DESCRIPTION"
+            ;;
+        esac
+    done
+}
+
+parse_params $@
+shift `expr $OPTIND - 1`
+var_not_empty_or_fail BUCKET_PATH "The bucket can't be empty!"
+S3CMD_PATH="`which s3cmd`"
+var_not_empty_or_fail S3CMD_PATH "s3cmd not found in path!"
+
+TMP_DIR="$TMP_DIR/`basename $0`.$$"
+mkdir "$TMP_DIR"
+print "'$TMP_DIR' will be used as temporary directory"
+
+while [[ ! -z "$1" ]]; do
+    print "Backing up directory '$1'"
+    if [[ -d "$1" ]]; then
+        BACKUPED_FILE="$TMP_DIR/`basename $1`_`date +%F_%H-%M-%S`.tar.bz2"
+        tar jcvf "$BACKUPED_FILE" "$1"
+        BACKUP_PATH=`echo "$BACKUP_PATH" | sed -e 's/^[/]*//' | sed -e 's/[/]*$//'`
+        if [[ ! -z "$BACKUP_PATH" ]] ; then   
+            BACKUP_PATH="$BACKUP_PATH"'/'
+        fi
+        if [[ ! -z "$DIRECTORY_BY_BACKUP" ]]; then
+            BACKUP_PATH="$BACKUP_PATH`basename $1`/"
+        fi
+        S3_FILE="$BUCKET_PATH/$BACKUP_PATH`basename $BACKUPED_FILE`"
+        $S3CMD_PATH put "$BACKUPED_FILE" "$S3_FILE"
+    else
+        print_error "$1 doesn't exists, skipping..."
+    fi
+    shift
+done
+
+rm -rf "$TMP_DIR"
+
+
+
